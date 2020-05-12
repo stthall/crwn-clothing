62fb6c359e6c8d5b1d3aafcdf08622e9c1f4ad18
stu <stthall@icloud.com>
2020-05-12 10:22:55 -0700

    implemented firebase utils, incl. ability to store auth'zd users into firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app'
-import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/firestore'
 
-const config={
+const firebaseConfig = {
     apiKey: "AIzaSyA2Dk5hv_sxBT73FiITccSy4kNn1YAMdco",
     authDomain: "crwn-db-bdd61.firebaseapp.com",
     databaseURL: "https://crwn-db-bdd61.firebaseio.com",
@@ -10,12 +10,33 @@ const config={
     storageBucket: "crwn-db-bdd61.appspot.com",
     messagingSenderId: "279002003467",
     appId: "1:279002003467:web:2ee6ae7628f0cf5729ef0e"
+  };
+
+firebase.initializeApp(firebaseConfig)
+export const firestore = firebase.firestore()
+export const auth = firebase.auth()
+
+export const createUserProfileDocument = async (userAuth, additionalData)=>{
+    if(!userAuth) return
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth
+        const createdAt = new Date()
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(e){
+            console.log('error creating user',e.message)
+        }
+    }
+    return userRef;
 }
 
-firebase.initializeApp(config)
-
-export const auth = firebase.auth()
-export const firstore = firebase.firestore()
 
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({prompt:'select_account'})
